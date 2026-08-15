24fc9dac5bc598211ad4cfdab69e50831c34fd18
JINGTING0714 <972554074@qq.com>
2026-08-15 22:25:14 +0800

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,15 +1,78 @@
-export default async function handler(request) {
+const fs = require("fs");
+const path = require("path");
 
-  return new Response(
-    JSON.stringify({
-      status:"api-ready",
-      message:"upload system online"
-    }),
-    {
-      headers:{
-        "content-type":"application/json"
-      }
+
+const config = require("../config.json");
+
+
+function getRepository(type){
+
+    if(type==="image"){
+        return config.repositories.image;
     }
-  );
+
+
+    if(type==="music" || type==="audio"){
+        return config.repositories.music;
+    }
+
+
+    if(type==="video"){
+        return config.repositories.video;
+    }
+
+
+    throw new Error("Unknown media type");
 
 }
+
+
+
+function createID(type){
+
+    const time = Date.now();
+
+    return type + "-" + time;
+
+}
+
+
+
+function createMediaData(file,type){
+
+
+    const repo = getRepository(type);
+
+
+    return {
+
+        id:createID(type),
+
+        name:file.name,
+
+        type:type,
+
+        size:file.size,
+
+
+        repository:repo.current,
+
+
+        data:repo.data,
+
+
+        status:"waiting"
+
+
+    };
+
+
+}
+
+
+
+module.exports={
+
+    createMediaData
+
+};
