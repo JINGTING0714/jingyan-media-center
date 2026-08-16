768511c6024dba0af8330df774f6206e4a071fbe
JINGTING0714 <972554074@qq.com>
2026-08-16 15:43:21 +0800

    Update rename.js

--- api/rename.js
+++ api/rename.js
@@ -1,4 +1,34 @@
-const fs=require("fs");
+const fs = require("fs");
+const path = require("path");
+
+
+const PINYIN = {
+
+"方":"fang",
+"大":"da",
+"同":"tong",
+
+"周":"zhou",
+"杰":"jie",
+"伦":"lun",
+
+"晴":"qing",
+"天":"tian",
+
+"你":"ni",
+"好":"hao",
+
+"的":"de",
+
+"音":"yin",
+"乐":"yue",
+
+"新":"xin",
+"歌":"ge"
+
+};
+
+
 
 
 
@@ -7,11 +37,8 @@ function loadConfig(){
     return JSON.parse(
 
         fs.readFileSync(
-
             "config.json",
-
             "utf8"
-
         )
 
     );
@@ -23,45 +50,100 @@ function loadConfig(){
 
 
 
+function getExtension(filename){
+
+    return path.extname(filename)
+    .toLowerCase();
+
+}
+
+
+
+
+
+
+function removeExtension(filename){
+
+    return path.basename(
+        filename,
+        path.extname(filename)
+    );
+
+}
+
+
+
+
+
+
+function convertChinese(text){
+
+
+    let result="";
+
+
+    for(
+        const char of text
+    ){
+
+        if(PINYIN[char]){
+
+            result +=
+            "-"
+            +
+            PINYIN[char];
+
+        }else{
+
+            result += char;
+
+        }
+
+    }
+
+
+    return result;
+
+}
+
+
+
+
+
+
 function normalizeName(name){
 
 
-    return name
+    let result =
+    convertChinese(name);
 
-    .toLowerCase()
 
+
+    result =
+    result
+    .toLowerCase();
+
+
+
+    result =
+    result
     .replace(
-
-        /[^\w\s-]/g,
-
-        ""
-
-    )
-
-    .replace(
-
-        /\s+/g,
-
+        /[^a-z0-9]+/g,
         "-"
-
-    )
-
-    .replace(
-
-        /-+/g,
-
-        "-"
-
-    )
-
-    .replace(
-
-        /^-|-$/g,
-
-        ""
-
     );
 
+
+
+    result =
+    result
+    .replace(
+        /^-+|-+$/g,
+        ""
+    );
+
+
+
+    return result;
 
 }
 
@@ -71,120 +153,133 @@ function normalizeName(name){
 
 
 
-
-function renameFile(
-
-    filename,
-
-    index
-
-){
+function getNextNumber(type){
 
 
     const config=
-
     loadConfig();
+
+
+    const file =
+    config.database[type];
 
 
 
     if(
-
-        !config.rename.enabled
-
+        !fs.existsSync(file)
     ){
 
-        return filename;
+        return "001";
 
     }
 
 
 
 
+    const list =
+    JSON.parse(
 
-    const ext=
-
-    filename
-
-    .split(".")
-
-    .pop()
-
-    .toLowerCase();
-
-
-
-
-
-    const original=
-
-    filename
-
-    .replace(
-
-        /\.[^/.]+$/,
-
-        ""
+        fs.readFileSync(
+            file,
+            "utf8"
+        )
 
     );
 
 
 
-
-
-    const name=
-
-    normalizeName(
-
-        original
-
-    );
-
-
-
-
-
-    const number=
-
-    String(index)
-
+    return String(
+        list.length+1
+    )
     .padStart(
-
         3,
-
         "0"
-
     );
 
 
+}
+
+
+
+
+
+
+
+
+function renameFile(filename){
+
+
+    const ext =
+    getExtension(filename);
+
+
+
+    const name =
+    removeExtension(filename);
+
+
+
+    const clean =
+    normalizeName(name);
+
+
+
+    let prefix="001";
+
+
+
+    if(
+        ext===".mp3" ||
+        ext===".wav" ||
+        ext===".flac"
+    ){
+
+        prefix=
+        getNextNumber("audio");
+
+    }
+
+
+
+    if(
+        ext===".jpg" ||
+        ext===".png" ||
+        ext===".webp"
+    ){
+
+        prefix=
+        getNextNumber("image");
+
+    }
+
+
+
+    if(
+        ext===".mp4" ||
+        ext===".webm"
+    ){
+
+        prefix=
+        getNextNumber("video");
+
+    }
 
 
 
     return (
 
-        number
-
+        prefix
         +
-
         "-"
-
         +
-
-        name
-
+        clean
         +
-
-        "."
-
-        +
-
         ext
 
     );
 
 
-
 }
-
 
 
 
@@ -198,7 +293,10 @@ module.exports={
     renameFile,
 
 
-    normalizeName
+    normalizeName,
+
+
+    convertChinese
 
 
 };
